8144744482e1fba744e89cbd95c4a44a394d8211
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-29 10:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission
+
+      // Get values from input fields
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+
+      // Construct the query string
+      var queryString = "name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+      // Update the URL text
+      document.getElementById("url").textContent = "https://localhost:8080/?" + queryString;
+    });
